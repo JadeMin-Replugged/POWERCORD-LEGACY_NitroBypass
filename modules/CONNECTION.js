7ecc732d5944f4188c2fce59d717353f4f4f4c0e
KlartNET <36400787+JadeMin@users.noreply.github.com>
2022-01-05 12:10:04 +0900

    code cleanup

--- modules/CONNECTION.js
+++ modules/CONNECTION.js
@@ -3,11 +3,12 @@ let isConnected = false;
 
 module.exports = class CONNECTION_MODULE {
 	constructor() {
-		this.isConnected = isConnected;
-		
 		this.afterLogin(()=> isConnected = true);
+		
+		this.isConnected = isConnected;
 	};
-
+	
+	
 	afterLogin(callback=Function, ...args){
 		return FluxDispatcher.subscribe('CONNECTION_OPEN', function(){
 			FluxDispatcher.unsubscribe('CONNECTION_OPEN', this);
