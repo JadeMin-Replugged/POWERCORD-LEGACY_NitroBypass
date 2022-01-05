6ac373f2cc4801b255af59bd5162b4d0043954cb
KlartNET <36400787+JadeMin@users.noreply.github.com>
2022-01-05 11:03:47 +0900

    Create CONNECTION.js

---
+++ NitroBypass/modules/CONNECTION.js
@@ -0,0 +1,19 @@
+const { Plugin } = require("powercord/entities");
+const { FluxDispatcher } = require("powercord/webpack");
+let isConnected = false;
+
+module.exports = class CONNECTION_MODULE {
+	constructor() {
+		this.isConnected = isConnected;
+		
+		this.afterLogin(()=> isConnected = true);
+	};
+
+	afterLogin(callback=Function, ...args){
+		return FluxDispatcher.subscribe('CONNECTION_OPEN', function(){
+			FluxDispatcher.unsubscribe('CONNECTION_OPEN', this);
+
+			callback(...args);
+		});
+	};
+};
