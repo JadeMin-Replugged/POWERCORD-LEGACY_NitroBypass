934c2fa44336e23b7eedb4d6706d66d4d3a6d464
KlartNET <dev1234@klartnet.ml>
2022-01-18 06:55:21 +0900

    Update and rename CONNECTION.js to Dispatcher.js

---
+++ modules/Dispatcher.js
@@ -0,0 +1,27 @@
+const { FluxDispatcher, getModule } = require("powercord/webpack");
+const { getCurrentUser } = getModule(["getCurrentUser"], false);
+
+
+let _isConnected = false;
+module.exports = class DISPATCHER {
+	constructor() {
+		this.afterLogin(()=> _isConnected = true);
+		//this.Flux = { ...FluxDispatcher };
+	};
+	
+	isConnected() {
+		return (getCurrentUser() !== undefined) || _isConnected;
+	};
+	once(eventName, callback=Function, ...args) {
+		const listener = function(){
+			FluxDispatcher.unsubscribe(eventName, listener);
+			
+			callback(...args);
+		};
+
+		return FluxDispatcher.subscribe(eventName, listener);
+	};
+	afterLogin(callback=Function, ...args) {
+		return this.once('CONNECTION_OPEN', ()=> callback(...args));
+	};
+};
